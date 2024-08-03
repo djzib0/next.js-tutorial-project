64720f2162f3b6782f097364e164309796797406
djzib0 <piotrsabatura@gmail.com>
2024-08-03 09:37:42 +0200

    added styles to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import styles from "./home.module.css"
+import Image from 'next/image'
 
 export default function Homepage() {
 
   return (
-    <div className=''>
+    <div className={styles.container}>
       <div className={styles.textContainer}>
-        text test
+        <h1
+          className='text-3xl font-bold'
+        >Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed facere magni neque ea rem. 
+        Tempora quasi nam nemo sapiente exercitationem.</p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
       </div>
-      <div className={styles.imageContainer}>
-        image test
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} priority/>
       </div>
     </div>
   )
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,13 @@
+import styles from "./footer.module.css"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Zib0</div>
+      <div className={styles.text}>
+        Zib0 creative thoughts agency © All rights reserved.
+      </div>
+    </div>
   )
 }
 
